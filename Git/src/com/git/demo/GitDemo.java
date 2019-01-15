6553f2fa80514fcfe90fbe3ef6b1a6a840d2fb40
ZhangXiaoxiang <1090510535@qq.com>
2019-01-15 14:57:49 +0800

    注释内容:这是第一次提交

---
+++ Git/src/com/git/demo/GitDemo.java
@@ -0,0 +1,13 @@
+package com.git.demo;
+
+public class GitDemo {
+	public static void main(String[] args) {
+		System.out.println("你好Git!");
+		System.out.println("测试xml");
+		System.out.println("测试properties!");
+		System.out.println("一般新版本是控制台支持中文,还有就是案例演示使用text文本的话,\r\n" + 
+				"默认不是UTF-8的设置(可以修改),所以到gitHub官网看到的中文乱码,但是更新下载下来又可以读取,\r\n" + 
+				"由于最终会使用eclipse或者idea提交代码,可以不用纠结乱码配置,只要记住能设置UTF8的地方\r\n" + 
+				"比如java配置文件,java项目,工作空间编码,全程UTF8就行了)");
+	}
+}
